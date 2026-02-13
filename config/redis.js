b5633170cfbd79f287dd998ba68becbfa5b51381
Abdur Rahman <abdurrahman.codes1.com>
2026-02-14 01:58:40 +0530

    redis setup complete

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,37 @@
+// import { createClient } from "redis";
+
+// const redisClient = createClient({
+//   url: process.env.REDIS_URL,
+//   username: "default",
+//   password:,
+// });
+
+// redisClient.on("error", (err) => {
+//   console.log("redis client error", err);
+//   process.exit(1);
+// });
+
+// await redisClient.connect();
+
+// export default redisClient;
+
 import { createClient } from "redis";
 
-const redisClient = createClient();
-
-redisClient.on("error", (err) => {
-  console.log("redis client error", err);
-  process.exit(1);
+const redisClient = createClient({
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_URL,
+    port: 18361,
+  },
 });
 
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
 await redisClient.connect();
+
+await redisClient.set("foo", "bar");
+const result = await redisClient.get("foo");
+console.log(result); // >>> bar
 
 export default redisClient;
