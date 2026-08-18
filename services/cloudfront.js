a037effb6498b5e4ab47362714253f65f0066313
Abdur Rahman <abdurrahman.codes1.com>
2026-08-18 10:09:20 +0530

    fix bug in url generation of cdn

--- services/cloudfront.js
+++ services/cloudfront.js
@@ -1,7 +1,6 @@
 import { getSignedUrl } from "@aws-sdk/cloudfront-signer"; // ESM
 const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY;
 const keyPairId = process.env.AWS_KEY_PAIR_ID;
-const dateLessThan = new Date(Date.now() + 1000 * 60 * 60).toISOString(); // any Date constructor compatible
 const cloudfrontDistributionDomain =
   process.env.CLOUDDRONT_DISTRIBUTION_DOOMAIN;
 
@@ -10,6 +9,7 @@ export const createCloudFrontGetSignedUrl = ({
   download = false,
   filename,
 }) => {
+  const dateLessThan = new Date(Date.now() + 1000 * 60 * 60).toISOString();
   const url = `https://${cloudfrontDistributionDomain}/${key}?response-content-disposition=${encodeURIComponent(`${download ? "attachment" : "inline"};filename=${filename}`)}`;
   const signedUrl = getSignedUrl({
     url,
